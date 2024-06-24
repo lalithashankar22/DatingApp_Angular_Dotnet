a52a7117b704ecbcd8af8666d8a893f7a3ccf3a6
Lalitha S <lalitha.s2@dedalus.eu>
2024-06-24 10:33:44 +0530

    three

---
+++ Dating_app/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Dating_app.DB_Context;
+using Dating_app.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating_app.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class UserController : Controller
+    {
+        private readonly DA_DBContextClass dbcontext;
+
+        public UserController(DA_DBContextClass dB_Context)
+        {
+            dbcontext = dB_Context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUserClass>>> GetAllUsers()
+        {
+            var users = await dbcontext.User.ToListAsync();
+            return users;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<AppUserClass>> GetSingleUser(int id) 
+        {
+            var user = await dbcontext.User.SingleOrDefaultAsync(x => x.Id == id);
+            return user;
+        }
+    }
+}
